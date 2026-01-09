a4eab9b1af4836120fc78a3abd7bf6eda258c48e
proxy-alt <proxy-alt@proxy-alt.dev>
2026-01-09 07:07:46 -0500

    Cleanup

--- public/storage/ag/g/2048/js/application.js
+++
@@ -1,4 +0,0 @@
-// Wait till the browser is ready to render the game (avoids glitches)
-window.requestAnimationFrame(() => {
-	new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
-});
